5812aeafac81ed1abc19c73b0e2fd330eb59c839
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2021-12-17 11:11:13 -0800

    fetching the jobs data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,32 @@ import { FaAngleDoubleRight } from "react-icons/fa";
 const url = "https://course-api.com/react-tabs-project";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    setLoading(true);
+
+    const response = await fetch(url, { mode: "cors" });
+    const newJobs = await response.json();
+
+    setLoading(false);
+    setJobs(newJobs);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>loading...</h1>
+      </section>
+    );
+  }
+
   return <h2>tabs project setup</h2>;
 }
 
